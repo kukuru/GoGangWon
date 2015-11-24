bed7f6e6bfaad32beebabb61946c33dd8cdec747
kukuru <comus1200@gmail.com>
2015-11-24 17:55:45 +0900

    add const value

--- app/src/main/java/com/nankuru/gogangwon/CommonValue.java
+++ app/src/main/java/com/nankuru/gogangwon/CommonValue.java
@@ -6,7 +6,10 @@ package com.nankuru.gogangwon;
 public class CommonValue {
     public static String gangwonUrl = "http://data.gwd.go.kr/apiservice";
     public static String keyValue = "/7a75527147636f6d3530744f497642";
-    public static String emptyHouse = "/gangwon-housing-empty_house";
     public static String valueType = "/json";
+    public static String emptyHouse = "/gangwon-housing-empty_house";
+    public static String hospital = "/medical_tour-medical";
+    public static String market ="/sgs-traditional_market";
+
 
 }
